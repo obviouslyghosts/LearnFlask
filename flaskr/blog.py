1291869e19c301a68945db58949331f35989d338
Ryan <obviouslyghosts@gmail.com>
2020-07-09 12:41:25 -0400

    tests

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -84,7 +84,8 @@ def update( id ):
             db.commit()
             return redirect( url_for( 'blog.index' ) )
 
-        return render_template( 'blog/update.html', post = post)
+    return render_template( 'blog/update.html', post = post)
+
 
 @bp.route( '/<int:id>/delete', methods = ( 'POST', ) )
 @login_required
